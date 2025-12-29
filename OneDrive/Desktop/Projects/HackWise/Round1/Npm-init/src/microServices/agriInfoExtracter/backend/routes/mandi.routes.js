7e79dbab437b0793e269fa2aeacf4d46d40ace5e
KondkariYusuf <115357529+KondkariYusuf@users.noreply.github.com>
2025-12-29 22:05:06 +0530

    Delete OneDrive/Desktop/Projects directory

--- OneDrive/Desktop/Projects/HackWise/Round1/Npm-init/src/microServices/agriInfoExtracter/backend/routes/mandi.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getCropPrice } from "../controllers/mandi.controller.js";
-
-const router = express.Router();
-
-router.get("/price", getCropPrice);
-
-export default router;
